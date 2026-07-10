69200e6b7874c7046bac29acc0d9509e76d151ea
Akbar Suriaganda <akbar.suriaganda@gmail.com>
2026-07-10 19:28:46 +0200

    box sprite

--- frontend/app/room/carton-box/carton-box.component.js
+++ frontend/app/room/carton-box/carton-box.component.js
@@ -2,18 +2,41 @@
 
 angular.module("mimiau.room").component("cartonBox", {
   templateUrl: "room/carton-box/carton-box.template.html",
-  controller: ["RoomGrid", CartonBoxController],
+  controller: ["RoomGrid", "CartonBoxSprite", CartonBoxController],
   controllerAs: "$ctrl",
 });
 
-function CartonBoxController(RoomGrid) {
+function CartonBoxController(RoomGrid, CartonBoxSprite) {
   var $ctrl = this;
 
   $ctrl.getStyles = function () {
     return {
       width: RoomGrid.CELL_SIZE + "px",
+      height: RoomGrid.CELL_SIZE + "px",
+      display: "flex",
+      alignItems: "end",
+    };
+  };
+
+  $ctrl.getVisualStyles = function () {
+    var sheetCols = CartonBoxSprite.SHEET_COLUMNS;
+    var sheetRows = CartonBoxSprite.SHEET_ROWS;
+    var spriteCol = CartonBoxSprite.SPRITE_COLUMN;
+    var spriteRow = CartonBoxSprite.SPRITE_ROW;
+
+    return {
+      width: RoomGrid.CELL_SIZE + "px",
       height: RoomGrid.CELL_SIZE * 2 + "px",
-      transform: "translateY(" + -RoomGrid.CELL_SIZE * 1.5 + "px)",
+      pointerEvents: "none",
+      backgroundImage: "url('assets/spritesheets/carton-box-sheet.png')",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: sheetCols * 100 + "% " + sheetRows * 100 + "%",
+      backgroundPosition:
+        (spriteCol / (sheetCols - 1)) * 100 +
+        "% " +
+        (spriteRow / (sheetRows - 1)) * 100 +
+        "%",
+      imageRendering: "pixelated",
     };
   };
 
--- frontend/app/room/room.constants.js
+++ frontend/app/room/room.constants.js
@@ -5,3 +5,11 @@ angular.module("mimiau.room").constant("RoomGrid", {
   COLUMNS: 10,
   ROWS: 5,
 });
+
+angular.module("mimiau.room").constant("CartonBoxSprite", {
+  SHEET_PATH: "assets/spritesheets/carton-box-sheet.png",
+  SHEET_COLUMNS: 6,
+  SHEET_ROWS: 3,
+  SPRITE_COLUMN: 0,
+  SPRITE_ROW: 0,
+});
