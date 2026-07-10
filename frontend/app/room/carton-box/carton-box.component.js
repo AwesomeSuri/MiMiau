baeed7ddb330855cb55497d2cc863663471b57d8
Akbar Suriaganda <akbar.suriaganda@gmail.com>
2026-07-10 19:43:19 +0200

    box idle anim

--- frontend/app/room/carton-box/carton-box.component.js
+++ frontend/app/room/carton-box/carton-box.component.js
@@ -2,12 +2,50 @@
 
 angular.module("mimiau.room").component("cartonBox", {
   templateUrl: "room/carton-box/carton-box.template.html",
-  controller: ["RoomGrid", "CartonBoxSprite", CartonBoxController],
+  controller: [
+    "$timeout",
+    "RoomGrid",
+    "CartonBoxSprite",
+    CartonBoxController,
+  ],
   controllerAs: "$ctrl",
 });
 
-function CartonBoxController(RoomGrid, CartonBoxSprite) {
+function CartonBoxController($timeout, RoomGrid, CartonBoxSprite) {
   var $ctrl = this;
+
+  var frameIndex = 0;
+  var frameTimeout = null;
+
+  $ctrl.spriteCol = 0;
+  $ctrl.spriteRow = 0;
+
+  $ctrl.$onInit = function () {
+    showFrame(frameIndex);
+    scheduleNextFrame();
+  };
+
+  $ctrl.$onDestroy = function () {
+    if (frameTimeout) {
+      $timeout.cancel(frameTimeout);
+    }
+  };
+
+  function showFrame(index) {
+    var frame = CartonBoxSprite.ANIMATION_FRAMES[index];
+    $ctrl.spriteCol = frame.col;
+    $ctrl.spriteRow = frame.row;
+  }
+
+  function scheduleNextFrame() {
+    var currentFrame = CartonBoxSprite.ANIMATION_FRAMES[frameIndex];
+
+    frameTimeout = $timeout(function () {
+      frameIndex = (frameIndex + 1) % CartonBoxSprite.ANIMATION_FRAMES.length;
+      showFrame(frameIndex);
+      scheduleNextFrame();
+    }, currentFrame.duration);
+  }
 
   $ctrl.getStyles = function () {
     return {
@@ -21,20 +59,18 @@ function CartonBoxController(RoomGrid, CartonBoxSprite) {
   $ctrl.getVisualStyles = function () {
     var sheetCols = CartonBoxSprite.SHEET_COLUMNS;
     var sheetRows = CartonBoxSprite.SHEET_ROWS;
-    var spriteCol = CartonBoxSprite.SPRITE_COLUMN;
-    var spriteRow = CartonBoxSprite.SPRITE_ROW;
 
     return {
       width: RoomGrid.CELL_SIZE + "px",
       height: RoomGrid.CELL_SIZE * 2 + "px",
       pointerEvents: "none",
-      backgroundImage: "url('assets/spritesheets/carton-box-sheet.png')",
+      backgroundImage: "url('" + CartonBoxSprite.SHEET_PATH + "')",
       backgroundRepeat: "no-repeat",
       backgroundSize: sheetCols * 100 + "% " + sheetRows * 100 + "%",
       backgroundPosition:
-        (spriteCol / (sheetCols - 1)) * 100 +
+        ($ctrl.spriteCol / (sheetCols - 1)) * 100 +
         "% " +
-        (spriteRow / (sheetRows - 1)) * 100 +
+        ($ctrl.spriteRow / (sheetRows - 1)) * 100 +
         "%",
       imageRendering: "pixelated",
     };
--- frontend/app/room/room.constants.js
+++ frontend/app/room/room.constants.js
@@ -10,6 +10,9 @@ angular.module("mimiau.room").constant("CartonBoxSprite", {
   SHEET_PATH: "assets/spritesheets/carton-box-sheet.png",
   SHEET_COLUMNS: 6,
   SHEET_ROWS: 3,
-  SPRITE_COLUMN: 0,
-  SPRITE_ROW: 0,
+  ANIMATION_FRAMES: [
+    { col: 0, row: 0, duration: 1000 },
+    { col: 1, row: 0, duration: 200 },
+    { col: 2, row: 0, duration: 200 },
+  ],
 });
